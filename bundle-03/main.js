1744e61ffa3bd8ea3e7293acf82d541c6f7d52ee
andreafontaneto <andrea.fontaneto@gmail.com>
2021-12-09 18:24:43 +0100

    bundle-03 - milestone 2 + correzioni

--- bundle-03/main.js
+++ bundle-03/main.js
@@ -84,3 +84,87 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+
+// filtrare un array di auto e stamparle in base al tipo di alimentazione
+// nell'array manca una virgola tra 2 oggetti e c'è un virgola di troppo dopo l'ultimo oggetto
+// nelle arrow function non ci vogliono le parentesi tonde dichiarando l'oggetto: (auto) diventa --> auto
+// nel 1° filter >= diventa => (perchè è un arrow function) e auto.type può essere 'benzina' oppure 'Benzina'
+// nel 2° filter auto.type può essere 'diesel' oppure 'Diesel' togliendo le parentesi graffe
+// nel 3° filter si cancellano le graffe, il return non ci va e ||(OR) diventa &&(AND)
+
+const cars = [
+    {
+        manufacturer: 'Ford',
+        model: 'Fiesta',
+        type: 'diesel'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'A1',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Golf',
+        type: 'Benzina'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Panda',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Multipla',
+        type: 'GPL'
+    },
+    {
+        manufacturer: 'Tesla',
+        model: 'Model 3',
+        type: 'elettrico'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Polo',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Ford',
+        model: 'Kuga',
+        type: 'Diesel'
+    },
+    {
+        manufacturer: 'Seat',
+        model: 'Ibiza',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'R8',
+        type: 'Benzina'
+    }
+];
+
+const gasolineCars = cars.filter( auto => 
+    auto.type === 'benzina' && auto.type === 'Benzina';
+);
+
+const dieselCars = cars.filter( auto => 
+    auto.type === 'diesel' && auto.type === 'Diesel';
+);
+
+const otherCars = cars.filter( auto => 
+    auto.type !== 'benzina' && auto.type !== 'diesel';
+);
+
+console.log('Auto a benzina');
+console.log('*******************************');
+console.log(gasolineCars);
+
+console.log('Auto a diesel');
+console.log('*******************************');
+console.log(dieselCars);
+
+console.log('Tutte le altre auto');
+console.log('*******************************');
+console.log(otherCars);
