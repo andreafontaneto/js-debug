8318a744bcefbdf71c77bd94e773cc8a86b63973
andreafontaneto <andrea.fontaneto@gmail.com>
2021-12-09 17:53:56 +0100

    bundle-02 correzioni

--- bundle-02/main.js
+++ bundle-02/main.js
@@ -27,6 +27,19 @@ checkAge();
 // "message" può variare quindi deve essere un "let"
 // nessun errore logico
 
+// CORREZIONE //
+function checkAge() {
+    const myAge = 32;
+    let message = '';
+
+    if (myAge < 18) {
+        message = `Sei troppo giovane! Hai ${myAge} anni!`;
+    } else {
+        message = 'Hai più di 18 anni!';
+    }
+}
+checkAge();
+
 // ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
@@ -37,6 +50,14 @@ printColorsNumber();
 // funzione che stampa il numero totale di colori nella palette
 // "colors.length" deve essere salvato in una variabile PRIMA del console.log
 // POI dal console.log tramite `` (template literal) richiamare la nuova variabile
+
+// CORREZIONE //
+function printColorsNumber() {
+    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+    const colorsNumber = colors.lenght;
+    console.log(`Nella mia palette ci sono ${colorsNumber} colori!`);
+}
+printColorsNumber();
 
 
 // ESERCIZIO 3
@@ -52,6 +73,15 @@ addNumbers();
 // "userNumber" deve essere uguale a parseInt(prompt('Inserisci un numero'))
 // trasformando così il numero inserito da stringa a numero effettivo
 // nessun errore logico
+
+// CORREZIONE //
+function addNumbers() {
+    const userNumber = parseInt(prompt('Inserisci un numero'));
+    const total = userNumber + 12;
+
+    console.log(`Il risultato finale è ${total}`);
+}
+addNumbers();
 
 
 // ESERCIZIO 4
@@ -78,6 +108,25 @@ checkAccess();
 // la variabile "grantAccess" è uguale (sia fuori che dentro all'if) ad un stringa 'true','false'
 // e non ad un booleano true/false
 // nessun errore logico
+
+// CORREZIONE //
+function checkAccess() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+    let grantAccess = false;
+
+    if (addresses.includes(userEmail)) {
+        grantAccess = true;
+    }
+
+    if (grantAccess === true) {
+        console.log('Accesso consentito!');
+    } else {
+        console.log('Accesso negato!');
+    }
+}
+checkAccess();
 
 
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
@@ -112,6 +161,32 @@ checkAccessImproved();
 // nel 1° IF posso scrivere " if (userEmail.length > 5 && email === userEmail)
 // senza fare un 2° IF
 // doppia ripetizione dell'if, per il resto è giusto
+
+// CORREZIONE //
+function checkAccessImproved() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+    let grantAccess = false;
+
+    for (let i = 0; i < addresses.length; i++) {
+      
+      const email = addresses[i];
+        
+      if (userEmail.length > 5 && email === userEmail) {
+        grantAccess = 'true';                  
+      }
+        
+    }
+
+    if (grantAccess) {
+        console.log('Accesso consentito!');
+    } else {
+        console.log('Accesso negato!');
+    }
+}
+checkAccessImproved();
 
 
 
